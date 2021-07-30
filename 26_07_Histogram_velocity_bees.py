a5ea71bf11c9b69538da2482a6a9b4c65d1c82f9
V4L321N <41680849+V4L321N@users.noreply.github.com>
2021-07-30 15:26:40 +0200

    Add files via upload

---
+++ 26_07_Histogram_velocity_bees.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import scipy.stats
+from math import log, floor
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+import matplotlib.mlab as mlab
+from scipy.optimize import curve_fit
+
+head = list(pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_x.csv")))
+
+#test_x = [60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60]
+#test_y = [30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30]
+testbee = "BT01A-1"
+
+""" CONSTANTS """
+#--------------------------------------------------------------------------------
+
+# D_r = 400
+# a = 0.4
+
+# D_r = 1000
+# a = 1
+
+#--------------------------------------------------------------------------------
+
+"""-begin------remove empty entires------------------------------------------"""
+def X_remove_NaN(item):
+    X_dataset_entropy = pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_x.csv"), columns=[item])
+    X_dataset_wo_NAN = []
+    for m in range(len(X_dataset_entropy)):
+        check = X_dataset_entropy.iloc[m].item()
+        if str(check) != "nan":
+            X_dataset_wo_NAN.append(check)
+    return X_dataset_wo_NAN
+
+def Y_remove_NaN(item):
+    Y_dataset_entropy = pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_y.csv"), columns=[item])
+    Y_dataset_wo_NAN = []
+    for m in range(len(Y_dataset_entropy)):
+        check = Y_dataset_entropy.iloc[m].item()
+        if str(check) != "nan":
+            Y_dataset_wo_NAN.append(check)
+    return Y_dataset_wo_NAN
+"""-end--------remove empty entires------------------------------------------"""
+
+
+# """-begin------calculate velocity for each time step-----------------"""
+# def calc_vel(item):
+#     list_vel = []
+#     X_dataset_wo_NAN = X_remove_NaN(item) #test_x
+#     Y_dataset_wo_NAN = Y_remove_NaN(item) #test_y
+#     n = len(X_dataset_wo_NAN) - 1
+#     timeline = np.linspace(0, n, n)
+#     for t in range(n):
+#         velocity = np.sqrt((X_dataset_wo_NAN[t + 1] - X_dataset_wo_NAN[t]) ** 2 + (Y_dataset_wo_NAN[t + 1] - Y_dataset_wo_NAN[t]) ** 2)
+#         list_vel.append(velocity)
+#     return list_vel
+# #plt.plot(calc_vel(testbee))
+# #plt.show()
+# """-end--------calculate velocity in for each time step-----------------"""
+
+"""-begin------calculate velocity (except zero) for each time step-----------------"""
+def calc_vel(item):
+    list_vel = []
+    X_dataset_wo_NAN = X_remove_NaN(item) #test_x
+    Y_dataset_wo_NAN = Y_remove_NaN(item) #test_y
+    n = len(X_dataset_wo_NAN) - 1
+    timeline = np.linspace(0, n, n)
+    for t in range(n):
+        velocity = np.sqrt((X_dataset_wo_NAN[t + 1] - X_dataset_wo_NAN[t]) ** 2 + (Y_dataset_wo_NAN[t + 1] - Y_dataset_wo_NAN[t]) ** 2)
+        threshold = 0.0
+        if velocity >= threshold:
+            list_vel.append(velocity)
+    return list_vel
+#plt.plot(calc_vel(testbee))
+#plt.show()
+"""-end--------calculate velocity  (except zero) in for each time step-----------------"""
+
+
+"""-begin--------calculate fourier transform of the velocity-----------------"""
+def velocity_FFT(item):
+    return np.fft.fft(calc_vel(item), norm="ortho") #either norm="ortho" here or divided by C_2 in PSD()
+"""-end--------calculate fourier transform of the velocity-------------------"""
+
+
+"""-begin--------calculate power spectral density of the FT------------------"""
+def PSD(item):
+    vel_FFT = velocity_FFT(item)
+    loop_L = round(len(vel_FFT)/2)
+    PSD_list = []
+    for i in range(loop_L):
+        C_2 = 1
+        absolute = C_2 * (vel_FFT[i] * np.conj(vel_FFT[i]))
+        PSD_list.append(absolute.real)
+    return PSD_list
+
+# def PSD(item):
+#     PSD_list = []
+#     for i in range(round(len(theta_FFT)/2)):
+#         PSD_list.append(theta_FFT[i].real ** 2 + theta_FFT[i].imag ** 2)
+#     return PSD_list
+"""-end----------calculate power spectral density of the angle FT------------"""
+
+for testbee in head:
+    plt.title(testbee)
+    plt.xlim(0, 6, 0.1)
+    #plt.ylim(0, 10)
+    plt.xlabel('velocity in cm/s')
+    plt.ylabel('occurence')
+    #plt.yscale('log')
+    plt.hist(calc_vel(testbee), bins=100, range=(0,6))#, density=True)
+    plt.savefig("FIG_vel_hist/" + str(testbee))
+    #plt.savefig("FIG_vel_hist/LOG_scale/LOG" + str(testbee))
+
+    plt.close()
+    #plt.show()
