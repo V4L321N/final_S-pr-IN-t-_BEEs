e096b35ea8b9dbcc4669217ab3b5669f08c46790
V4L321N <41680849+V4L321N@users.noreply.github.com>
2021-09-07 14:50:38 +0200

    Add files via upload

---
+++ 05_09_ALL_WALKS_t1_of_DeltaT.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import scipy.stats
+from scipy.stats import norm
+from math import log, floor
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+head = list(pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_x.csv")))
+
+#test_x = [60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60]
+#test_y = [30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30]
+#head = ["BT05A-2"]
+
+""" CONSTANTS """
+#-------------------------------------------------------------------------------
+# D_r = 400
+# a = 0.4
+# D_r = 1000
+# a = 1
+#-------------------------------------------------------------------------------
+
+"""-begin------remove empty entires------------------------------------------"""
+def X_remove_NaN(item):
+    X_dataset_entropy = pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_x.csv"), columns=[item])
+    X_dataset_wo_NAN = []
+    for m in range(len(X_dataset_entropy)):
+        check = X_dataset_entropy.iloc[m].item()
+        if str(check) != "nan":
+            X_dataset_wo_NAN.append(check)
+    return X_dataset_wo_NAN
+
+def Y_remove_NaN(item):
+    Y_dataset_entropy = pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_y.csv"), columns=[item])
+    Y_dataset_wo_NAN = []
+    for m in range(len(Y_dataset_entropy)):
+        check = Y_dataset_entropy.iloc[m].item()
+        if str(check) != "nan":
+            Y_dataset_wo_NAN.append(check)
+    return Y_dataset_wo_NAN
+
+def T_remove_NaN(item):
+    T_dataset = pd.DataFrame(data=pd.read_csv("data_bee_types/temperature_runs.csv"), columns=[item])
+    T_dataset_wo_NAN = []
+    for m in range(len(T_dataset)):
+        check = T_dataset.iloc[m].item()
+        if str(check) != "nan":
+            T_dataset_wo_NAN.append(check)
+    return T_dataset_wo_NAN
+"""-end--------remove empty entires------------------------------------------"""
+
+"""-begin--------calculate probability for spending time at a temperature-----------------"""
+def probability_T(item):
+    T_dataset_wo_NAN = T_remove_NaN(item)
+    T_minmax_data = pd.DataFrame(data=pd.read_csv("data_bee_types/temperature_variables.csv"), columns=[item])
+    T_ideal = T_minmax_data.iloc[1].item()
+    prob_dataset = []
+    for T in range(len(T_dataset_wo_NAN)):
+        prob_dataset.append(np.abs(T_dataset_wo_NAN[T] - T_ideal))
+    return prob_dataset
+"""-end--------calculate probability for spending time at a temperature-------------------"""
+
+"""-begin------calculate duration stopping-----------------"""
+def calc_dur(item):
+    list_vel = []
+    X_dataset_wo_NAN = X_remove_NaN(item) #test_x
+    Y_dataset_wo_NAN = Y_remove_NaN(item) #test_y
+    n = len(X_dataset_wo_NAN) - 1
+    for t in range(n):
+        velocity = np.sqrt((X_dataset_wo_NAN[t + 1] - X_dataset_wo_NAN[t]) ** 2 + (Y_dataset_wo_NAN[t + 1] - Y_dataset_wo_NAN[t]) ** 2)
+        list_vel.append(velocity)
+    stop_vel = 0.5
+    duration_walk = 0
+    walk_durations = []
+    for v in range(len(list_vel[:-1])):
+        if list_vel[v] >= stop_vel:
+            duration_walk += 1
+            if list_vel[v+1] < stop_vel:
+                walk_durations.append(duration_walk)
+                duration_walk = 0
+            elif v == len(list_vel[:-2]):
+                walk_durations.append(duration_walk)
+    return walk_durations
+#print(calc_vel(testbee))
+#plt.show()
+"""-end--------calculate duration stopping-----------------"""
+
+# for testbee in head:
+#     print()
+ALL_walk_durations = []
+
+for testbee in head:
+    ALL_walk_durations.extend(calc_dur(testbee))
+
+plt.xlabel('walking times [s]')
+plt.ylabel('counts')
+plt.hist(ALL_walk_durations, bins=100, range=(0,100))#, density=True)
+plt.show()
---
+++ 07_09_ALL_walks_t1_of_DeltaT.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import scipy.stats
+from scipy.stats import norm
+from math import log, floor
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+head = list(pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_x.csv")))
+
+#test_x = [60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10, 0, 10, 20, 30, 40, 50, 60]
+#test_y = [30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30]
+#head = ["BT05A-2"]
+
+""" CONSTANTS """
+#-------------------------------------------------------------------------------
+# D_r = 400
+# a = 0.4
+# D_r = 1000
+# a = 1
+#-------------------------------------------------------------------------------
+
+"""-begin------remove empty entires------------------------------------------"""
+def X_remove_NaN(item):
+    X_dataset_entropy = pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_x.csv"), columns=[item])
+    X_dataset_wo_NAN = []
+    for m in range(len(X_dataset_entropy)):
+        check = X_dataset_entropy.iloc[m].item()
+        if str(check) != "nan":
+            X_dataset_wo_NAN.append(check)
+    return X_dataset_wo_NAN
+
+def Y_remove_NaN(item):
+    Y_dataset_entropy = pd.DataFrame(data=pd.read_csv("data_bee_types/LS_spatial_D_y.csv"), columns=[item])
+    Y_dataset_wo_NAN = []
+    for m in range(len(Y_dataset_entropy)):
+        check = Y_dataset_entropy.iloc[m].item()
+        if str(check) != "nan":
+            Y_dataset_wo_NAN.append(check)
+    return Y_dataset_wo_NAN
+
+def T_remove_NaN(item):
+    T_dataset = pd.DataFrame(data=pd.read_csv("data_bee_types/temperature_runs.csv"), columns=[item])
+    T_dataset_wo_NAN = []
+    for m in range(len(T_dataset)):
+        check = T_dataset.iloc[m].item()
+        if str(check) != "nan":
+            T_dataset_wo_NAN.append(check)
+    return T_dataset_wo_NAN
+"""-end--------remove empty entires------------------------------------------"""
+
+"""-begin--------calculate probability for spending time at a temperature-----------------"""
+def probability_T(item):
+    T_dataset_wo_NAN = T_remove_NaN(item)
+    T_minmax_data = pd.DataFrame(data=pd.read_csv("data_bee_types/temperature_variables.csv"), columns=[item])
+    T_ideal = T_minmax_data.iloc[1].item()
+    prob_dataset = []
+    for T in range(len(T_dataset_wo_NAN)):
+        prob_dataset.append(np.abs(T_dataset_wo_NAN[T] - T_ideal))
+    return prob_dataset
+"""-end--------calculate probability for spending time at a temperature-------------------"""
+
+"""-begin------calculate duration stopping-----------------"""
+def calc_dur(item):
+    list_vel = []
+    X_dataset_wo_NAN = X_remove_NaN(item) #test_x
+    Y_dataset_wo_NAN = Y_remove_NaN(item) #test_y
+    n = len(X_dataset_wo_NAN) - 1
+    for t in range(n):
+        velocity = np.sqrt((X_dataset_wo_NAN[t + 1] - X_dataset_wo_NAN[t]) ** 2 + (Y_dataset_wo_NAN[t + 1] - Y_dataset_wo_NAN[t]) ** 2)
+        list_vel.append(velocity)
+
+    T_dataset_wo_NAN = T_remove_NaN(item)
+    T_minmax_data = pd.DataFrame(data=pd.read_csv("data_bee_types/temperature_variables.csv"), columns=[item])
+    T_ideal = T_minmax_data.iloc[1].item()
+
+    stop_vel = 0.4
+    duration_walk = 0
+    all_walk_durations = []
+    walk_temperatures = []
+    mean_delta_temp = []
+    for v in range(len(list_vel[:-1])):
+        if list_vel[v] >= stop_vel:
+            duration_walk += 1
+            walk_temperatures.append(T_dataset_wo_NAN[v])
+            if list_vel[v+1] < stop_vel:
+                all_walk_durations.append(duration_walk)
+                mean_delta_temp.append(np.abs(np.mean(walk_temperatures) - T_ideal))
+                duration_walk = 0
+                walk_temperatures = []
+            elif v == len(list_vel[:-2]):
+                all_walk_durations.append(duration_walk)
+                mean_delta_temp.append(np.abs(np.mean(walk_temperatures) - T_ideal))
+    return all_walk_durations, mean_delta_temp
+#print(calc_vel(testbee))
+#plt.show()
+"""-end--------calculate duration stopping-----------------"""
+
+# for testbee in head:
+#     print()
+ALL_WALKS_at_dT = []
+ALL_TEMPS_at_walk = []
+for testbee in head:
+    walking_times, mean_temps = calc_dur(testbee)
+    ALL_WALKS_at_dT.extend(walking_times)
+    ALL_TEMPS_at_walk.extend(mean_temps)
+#plt.hist(ALL_WALKS_at_dT, bins=100)
+plt.scatter(ALL_TEMPS_at_walk, ALL_WALKS_at_dT)
+plt.xlabel("delta T")
+plt.ylabel("stop duration")
+#plt.xscale('log')
+#plt.yscale('log')
+plt.show()
